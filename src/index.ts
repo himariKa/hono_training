2c931d6571717ce8c3ee4a795d4c7529ca4138c9
鰈﨑ひまり <k.himari.1349@gmail.com>
2025-03-08 22:11:30 +0900

    first commit!!!!

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/hello', (c) => {
+  return c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  })
+})
+
+app.get('/posts/:id', (c) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+  return c.text(`You want to see ${page} of ${id}`)
+})
+
+app.post('/posts', (c) => c.text('Created!', 201))
+app.delete('/posts/:id', (c) =>
+  c.text(`${c.req.param('id')} is deleted!`)
+)
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+const View = () => `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Hono Page</title>
+    </head>
+    <body>
+      <h1>Hello Hono!</h1>
+    </body>
+  </html>
+`
+
+app.get('/page', (c) => {
+  return c.html(View())
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
